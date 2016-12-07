35e34e1c0a01a57d7c594338a3fc6b953fae407b
plpl24 <f.tomo.136@gmail.com>
2016-12-07 11:15:06 +0900

    Test

--- app/src/main/java/com/plplsent/battleshooting/Activity/StartActivity.java
+++ app/src/main/java/com/plplsent/battleshooting/Activity/StartActivity.java
@@ -1,7 +1,9 @@
-package com.plplsent.battleshooting;
+package com.plplsent.battleshooting.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.plplsent.battleshooting.R;
 
 public class StartActivity extends AppCompatActivity {
 
