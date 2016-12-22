94660a7355c439d42cf6b6c5eb667f45238ca1c1
plpl24 <f.tomo.136@gmail.com>
2016-12-22 11:31:35 +0900

    Add files via upload

---
+++ app/src/main/java/com/plplsent/battleshooting/Game/Event/BaseEvent.java
@@ -0,0 +1,16 @@
+package com.plplsent.battleshooting.Game.Event;
+
+import com.plplsent.battleshooting.Game.Field;
+
+
+abstract class BaseEvent implements Event {
+    protected final Field.Team TEAM;
+
+    BaseEvent(Field.Team team) {
+        TEAM = team;
+    }
+    Field.Team getTeam(){
+	return team;
+    }
+
+}
