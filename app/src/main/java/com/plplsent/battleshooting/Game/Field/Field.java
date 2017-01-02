28300c41791b0d8e57792c97806c2005f0a8b019
plpl24 <f.tomo.136@gmail.com>
2017-01-02 16:47:23 +0900

    Update Field.java

--- app/src/main/java/com/plplsent/battleshooting/Game/Field/Field.java
+++ app/src/main/java/com/plplsent/battleshooting/Game/Field/Field.java
@@ -5,15 +5,18 @@ import com.plplsent.battleshooting.Utils.DPoint;
 import com.plplsent.battleshooting.Game.Entity.Bullets.BulletGroup;
 import com.plplsent.battleshooting.Game.Entity.Entity;
 import com.plplsent.battleshooting.Game.Entity.Players.PlayerGroup;
-import com.plplsent.battleshooting.Game.Entity.TeamGroup.TeamGroup;
+import com.plplsent.battleshooting.Game.Entity.Tea
+
+mGroup.TeamGroup;
 
 public class Field {
     private final PlayerGroup players;
-    private final BulletGroup bulletGroup;
-
+    private final BulletGroup bulletGroup;   
+    
+    private final DPoint FIELD_POINT = new DPoint(500,500);
     private Field() {
         players = new PlayerGroup();
-        bulletGroup = new BulletGroup(500,500);
+        bulletGroup = new BulletGroup(FIELD_POINT);
     }
 
     public Entity getPlayer(TeamGroup.Team team) {
